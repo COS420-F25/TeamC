ba6c5c99d8899eb09dd4f1b791550015c413f085
PlasmaticJewel <123777068+PlasmaticJewel@users.noreply.github.com>
2025-11-17 17:10:13 -0500

    Added userSettings props initialization

--- src/Settings.tsx
+++ src/Settings.tsx
@@ -1,4 +1,5 @@
-//This shpuld create a visual popup state that allows the user to choose what their settings preference will be
+import React, {useState} from 'react';
+//This should create a visual popup state that allows the user to choose what their settings preference will be
 
 //define the settings object to be used
 interface userSettings{
