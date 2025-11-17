10a4f2c057e25f79d7ccf1fd7bd0e4c7cd8b1284
PlasmaticJewel <123777068+PlasmaticJewel@users.noreply.github.com>
2025-11-17 13:34:58 -0500

    Added settings file containing a userSettings object constructor.

---
+++ src/Settings.tsx
@@ -0,0 +1,9 @@
+//This shpuld create a visual popup state that allows the user to choose what their settings preference will be
+
+//define the settings object to be used
+interface userSettings{
+    theme: 'light' | 'dark';
+    pushNotifications: boolean
+    fontSize: number
+    fonts: String
+}
---
+++ src/SettingsTests.tsx
@@ -0,0 +1,11 @@
+//This will be where the settings will be tested
+
+//first test that there is a settings button
+
+//text if the settings button can be pressed
+
+//check to see if the window appeared
+
+//check to see if light/darkmode works
+
+//check to see that the font size works
